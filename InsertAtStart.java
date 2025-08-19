485301ba5a243867de28e68ae133127983e3e836
prasanna8949 <subbusubbarayudu211@gmail.com>
2025-08-19 14:42:38 +0530

    Add files via upload

--- InsertAtStart.java
+++ InsertAtStart.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+
 public class InsertAtStart {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int[] newArr = new int[arr.length + 1];
+        newArr[0] = 5;
+
+        for (int i = 0; i < arr.length; i++)
+            newArr[i + 1] = arr[i];
+
+        System.out.println(Arrays.toString(newArr));
+    }
 }
